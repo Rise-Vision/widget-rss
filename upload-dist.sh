360efd7bc0f7c77bac50f7b658072ab6b5095b5e
santiagonoguez <santiago@nul-unu.com>
2018-08-20 09:22:42 -0500

    use rsync instead of cp

--- upload-dist.sh
+++ upload-dist.sh
@@ -1,4 +1,4 @@
 cp -r dist rss
-gsutil -m cp -r rss gs://install-versions.risevision.com/widgets/
+gsutil rsync -d -r rss gs://install-versions.risevision.com/widgets/
 gsutil -m acl -r ch -u AllUsers:R gs://install-versions.risevision.com/widgets/rss
 gsutil -m setmeta -r -h Cache-Control:private,max-age=0 gs://install-versions.risevision.com/widgets/rss
